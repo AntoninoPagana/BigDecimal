c2221bf4f71b910a9e2d699ae187e8161cad75ac
Antonino <pagana899@gmail.com>
2024-01-16 16:48:55 +0100

    Esercizio Completo 2

--- OperazioneAritmeticaEnum.java
+++ OperazioneAritmeticaEnum.java
@@ -1,15 +1,8 @@
 public enum OperazioneAritmeticaEnum {
-    ADDIZIONE("+"),
-    SOTTRAZIONE("-"),
-    MOLTIPLICAZIONE("*"),
-    DIVISIONE("/"),
-    MIN("min"),
-    MAX("MAX");
-
-    private final String operazione;
-
-    OperazioneAritmeticaEnum(String operazione) {
-        this.operazione = operazione;
-    }
-
+    ADDIZIONE,
+    SOTTRAZIONE,
+    MOLTIPLICAZIONE,
+    DIVISIONE,
+    MIN,
+    MAX;
 }
